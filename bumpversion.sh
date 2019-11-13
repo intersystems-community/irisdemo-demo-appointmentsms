8d8c8db9f9d97714898787ed98bcb57b903cd470
Amir Samary <amirsamary@gmail.com>
2019-11-13 16:45:02 -0500

    Fixing bumpversion

--- bumpversion.sh
+++ bumpversion.sh
@@ -63,7 +63,7 @@ if [ -f VERSION ]; then
     # belonging to this repository. References to other images such as Spark and Zeppelin 
     # will be let alone.
     #
-    sed -E -i '' "s;(intersystemsdc/irisdemo-demo-appointmentsms:.+)-version-[0-9][0-9.]*;\1-version-$INPUT_STRING;g" ./README.md
+    sed -E -i '' "s;(intersystemsdc/irisdemo-demo-appointmentsms):version-[0-9][0-9.]*;\1:version-$INPUT_STRING;g" ./README.md
 
     echo "## $INPUT_STRING ($NOW)" > tmpfile
     git log --pretty=format:"  - %s" "v$BASE_STRING"...HEAD >> tmpfile
