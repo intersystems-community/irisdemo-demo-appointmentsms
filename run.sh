34dea1dea245e71139832fe2afe1f651a8c7222d
Amir Samary <amirsamary@gmail.com>
2019-11-13 16:40:02 -0500

    Adding new line

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ printf "\n\nRunning container. "
 printf "\n\t Management portal is on http://localhost:52773/csp/sys/UtilHome.csp"
 printf "\n\t Simulator page is on http://localhost:52773/csp/appint/demo.csp"
 printf "\n\t InterSystems IRIS Web Server Port is on 52773."
-printf "\n\t InterSystems IRIS Super Server Port is on 51773."
+printf "\n\t InterSystems IRIS Super Server Port is on 51773.\n\n"
 
 docker run --init -it --rm \
     -p 51773:51773 -p 52773:52773 \
