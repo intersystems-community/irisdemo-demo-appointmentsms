e7e5fa0c7543ce05409d305470b4194ce129f7e5
isc-virajesh <Vineeth.Rajesh@intersystems.com>
2022-10-20 14:07:21 -0400

    Spelling Mistake

--- build-x86.sh
+++ build-x86.sh
@@ -8,5 +8,5 @@ VERSION=`cat ./VERSION`
 docker buildx build --platform linux/amd64 -f ./amd64.dockerfile -t ${DOCKER_REPO}:amd64-version-${VERSION} .
 docker tag ${DOCKER_REPO}:amd64-version-${VERSION} ${DOCKER_REPO}:amd64-latest
 
-docker buildx build --platform linux/amd64 -f ./amd64.student.docker -t ${DOCKER_REPO}:amd64-student-version-${VERSION} .
+docker buildx build --platform linux/amd64 -f ./amd64.student.dockerfile -t ${DOCKER_REPO}:amd64-student-version-${VERSION} .
 docker tag ${DOCKER_REPO}:amd64-student-version-${VERSION} ${DOCKER_REPO}:amd64-student-latest
